84de66fe9d160465a373bce1e82a10ef7827bfb1
Vladimir <36823172+Vladimir-carr@users.noreply.github.com>
2020-08-19 23:26:20 +0300

    Update Level1.java

--- Level1.java
+++ Level1.java
@@ -1,7 +1,11 @@
 public class Level1
 {
-    public static int squirrel(int N)
-      {
+    public static int squirrel(int N) {
+        int a = fac(N);
+        int b = Integer.parseInt(Integer.toString(a).substring(0,1));
+        return b;
+    }
+    public static int fac(int N) {
         return (N == 1) ? 1 : N * squirrel(N - 1);
-      }
+    }
 }
