557604a2d4ef12a30910c006259a6c134881f54e
Vladimir <36823172+Vladimir-carr@users.noreply.github.com>
2020-09-02 22:45:56 +0300

    Update Level1_2.java

--- Level1_2.java
+++ Level1_2.java
@@ -7,25 +7,9 @@ public class Level1
             asc[i] = Tele[i];
             desc[i] = Tele[i];
         }
-        for (int i = 0; i < asc.length - 1; i++) {
-            for (int j = asc.length - 1; j > i; j--) {
-                if (asc[j - 1] > asc[j]) {
-                    int tmp = asc[j - 1];
-                    asc[j - 1] = asc[j];
-                    asc[j] = tmp;
-                }
-            }
-        }
-        
-        for (int i = 0; i < desc.length - 1; i++) {
-            for (int j = desc.length - 1; j > i; j--) {
-                if (desc[j - 1] < desc[j]) {
-                    int tmp = desc[j - 1];
-                    desc[j - 1] = desc[j];
-                    desc[j] = tmp;
-                }
-            }
-        }
+        BubbleSortAcs(asc);
+        BubbleSortDesc(desc);
+
         int[] total = new int[N];
         int middle = asc.length / 2;
         int i = 0;
@@ -42,5 +26,27 @@ public class Level1
         }
 
         return total;
+    }
+    public static void BubbleSortAcs(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = arr.length - 1; j > i; j--) {
+                if (arr[j - 1] > arr[j]) {
+                    int tmp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+        }
+    }
+    public static void BubbleSortDesc(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = arr.length - 1; j > i; j--) {
+                if (arr[j - 1] < arr[j]) {
+                    int tmp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+        }
     }
 }
